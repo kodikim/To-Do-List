f2f46fe99e076abafdcc4835b2778ecbb03f8fe7
Kodi <c23ck@dalton.org>
2020-06-22 10:12:29 -0400

    Initial Commit

---
+++ To-Do List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  To-Do List
+//
+//  Created by Sherry Hsiung on 6/22/20.
+//  Copyright © 2020 Kodi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
